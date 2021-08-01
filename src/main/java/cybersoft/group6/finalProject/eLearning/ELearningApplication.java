d0748e7b10fe33b116dd097690b46f6569b98ba9
BobbyBui <kelvinkhoabui@gmail.com>
2021-07-31 21:25:30 -0400

    Initial commit

---
+++ src/main/java/cybersoft/group6/finalProject/eLearning/ELearningApplication.java
@@ -0,0 +1,13 @@
+package cybersoft.group6.finalProject.eLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cybersoft/group6/finalProject/eLearning/ELearningApplicationTests.java
@@ -0,0 +1,13 @@
+package cybersoft.group6.finalProject.eLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
