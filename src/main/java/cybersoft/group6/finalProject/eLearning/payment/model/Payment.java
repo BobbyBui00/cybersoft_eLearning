46e6633674892ad4e83c9b6c5347f13a843c665b
BobbyBui <kelvinkhoabui@gmail.com>
2021-08-09 20:50:02 -0400

    Finish Repository and Service
    - create repository and service for Payment model
    - create repository and service for User model
    - add validation for Payment and User model

--- src/main/java/cybersoft/group6/finalProject/eLearning/payment/model/Payment.java
+++ src/main/java/cybersoft/group6/finalProject/eLearning/payment/model/Payment.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
 import cybersoft.group6.finalProject.eLearning.commondata.model.AbstractEntity;
 import cybersoft.group6.finalProject.eLearning.user.model.User;
@@ -19,14 +20,19 @@ import lombok.Setter;
 @Setter
 public class Payment extends AbstractEntity {
 
+	@NotBlank(message = "{payment.cardNumber.not-blank}")
 	private String cardNumber;
 	
+	@NotBlank(message = "{payment.cardHolder.not-blank}")
 	private String cardHolder;
 	
+	@NotBlank(message = "{payment.billingAddress.not-blank}")
 	private String billingAddress;
 	
+	@NotBlank(message = "{payment.cvv.not-blank}")
 	private String cvv;
 	
+	@NotBlank(message = "{payment.dateExpired.not-blank}")
 	private LocalDateTime dateExpired;
 	
 	@ManyToOne(fetch = FetchType.LAZY)
---
+++ src/main/java/cybersoft/group6/finalProject/eLearning/payment/service/PaymentService.java
@@ -0,0 +1,8 @@
+package cybersoft.group6.finalProject.eLearning.payment.service;
+
+import cybersoft.group6.finalProject.eLearning.commonData1.GenericService;
+import cybersoft.group6.finalProject.eLearning.payment.model.Payment;
+
+public interface PaymentService extends GenericService<Payment, Long> {
+
+}
