16f74eae87f8581d4415ba9a4b6a43dca4a7db60
BobbyBui <kelvinkhoabui@gmail.com>
2021-08-25 13:20:19 -0400

    solve cross origin 3

--- src/main/java/cybersoft/group6/finalProject/eLearning/config/WebSecurityConfig.java
+++ src/main/java/cybersoft/group6/finalProject/eLearning/config/WebSecurityConfig.java
@@ -5,19 +5,48 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 {
+	
+	
 
-
+	protected List<String> setUpAllowMethods() {
+		
+		List<String> allowMethods = new ArrayList<String>();
+		
+		allowMethods.add("GET");
+		allowMethods.add("POST");
+		allowMethods.add("PUT");
+		allowMethods.add("DELETE");
+		allowMethods.add("PUT");
+		allowMethods.add("OPTIONS");
+		allowMethods.add("PATCH");
+		allowMethods.add("DELETE");
+		
+		return allowMethods;
+	}
+	
+	protected List<String> setUpAllowOrigins() {
+		
+		List<String> allowOrigin = new ArrayList<String>();
+		
+		allowOrigin.add("*");
+		
+		return allowOrigin;
+		
+	}
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+    	
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 //        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-        corsConfiguration.setAllowedOrigins(List.of("*"));
-        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
+        corsConfiguration.setAllowedOrigins(setUpAllowOrigins());
+        corsConfiguration.setAllowedMethods(setUpAllowMethods());
         corsConfiguration.setAllowCredentials(false);
 //        corsConfiguration.setExposedHeaders(List.of("Authorization"));
         // You can customize the following part based on your project, it's only a sample
